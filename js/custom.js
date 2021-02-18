b9d1a69be8b33fb83895229463b3c76b7649371a
thebobak <bshafiei@gmail.com>
2021-02-17 23:25:58 -0500

    Example html

---
+++ js/custom.js
@@ -0,0 +1,15 @@
+(function() {
+	var vjs = videojs('hapyak-player-157199-8825');
+
+	hapyak.viewer({
+		apiKey: "4c5b39201f724da898f5",
+		projectId: 390557,
+		controls: true,
+		resetVariables: true,
+
+		player: vjs,
+		videoType: "html5",
+		playerType: "videojs4",
+		autoplay: false
+	});
+})();
